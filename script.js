6b1699fa27c8605771f9f418101ceeb591520cf2
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-07-11 16:52:11 +0200

    modified in function not yet completed.

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-let btnStart = document.getElementById("btnSt");
-let btnStop = document.getElementById("btnSp");
-let btnReset = document.getElementById("btnRs");
+const getTime = document.getElementById('timer');
 
-let getTime = document.getElementById("timer");
+
+let btnStart = document.getElementById('btnSt');
+let btnStop = document.getElementById('btnSp');
+let btnReset = document.getElementById('btnRs');
+
+//timeRun();
 
 btnStart.addEventListener('click',()=>{
   return timeRun();
 })
 
 btnStop.addEventListener('click',()=>{
-    
+  return timeRun();
+   
 })
 
 btnReset.addEventListener('click',()=>{
+  return timeRun();
     
 })
 
@@ -20,7 +25,6 @@ let interval;       //to start or stop the time loop.
 let timeInterval = 1500;   // 1500 sec = 25min
 
 function timeRun(){
-    console.log(getTime);
     let minutes = Math.floor(timeInterval/60);
     let seconds = timeInterval % 60;
     let fullTime = '${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}';
