729dbdb774c8795b9a945eeef2a43faa8c748c0d
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-07-11 15:28:45 +0200

    initially created variables and took elements from html page.

---
+++ script.js
@@ -0,0 +1,6 @@
+let btnStart = document.getElementById("btnSt");
+let btnStop = document.getElementById("btnSp");
+let btnReset = document.getElementById("btnRs");
+
+let getTime = document.getElementById("timer");
+
