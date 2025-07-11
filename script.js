dadc86d329a68321314743e1500131fc30dc07c3
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-07-11 15:50:05 +0200

    added new function called timeRun and variables.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let btnReset = document.getElementById("btnRs");
 let getTime = document.getElementById("timer");
 
 btnStart.addEventListener('click',()=>{
-
+  return timeRun();
 })
 
 btnStop.addEventListener('click',()=>{
@@ -15,3 +15,13 @@ btnStop.addEventListener('click',()=>{
 btnReset.addEventListener('click',()=>{
     
 })
+
+let interval;       //to start or stop the time loop.
+let timeInterval = 1500;   // 1500 sec = 25min
+
+function timeRun(){
+    console.log(getTime);
+    let minutes = Math.floor(timeInterval/60);
+    let seconds = timeInterval % 60;
+
+}
