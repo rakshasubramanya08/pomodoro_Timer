a5fb3615261bc96e21878070980cec1805b347dc
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-07-11 17:14:50 +0200

    added, modified functions and completed full.

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
-const getTime = document.getElementById('timer');
+const getTime = document.getElementById(`timer`);
 
 
 const btnStart = document.getElementById('btnSt');
 const btnStop = document.getElementById('btnSp');
 const btnReset = document.getElementById('btnRs');
 
-//timeRun();
+btnStart.addEventListener('click',startTime);
 
-btnStart.addEventListener('click',()=>{
-  return timeRun();
-})
+btnStop.addEventListener('click',stopTimer)
 
-function startTime(){
-    
-}
-
-btnStop.addEventListener('click',()=>{
-  return timeRun();
-   
-})
-
-btnReset.addEventListener('click',()=>{
-  return timeRun();
-    
-})
+btnReset.addEventListener('click',resetTimer)
 
 let interval;       //to start or stop the time loop.
 let timeInterval = 1500;   // 1500 sec = 25min
@@ -31,7 +17,32 @@ let timeInterval = 1500;   // 1500 sec = 25min
 function timeRun(){
     let minutes = Math.floor(timeInterval/60);
     let seconds = timeInterval % 60;
-    let fullTime = '${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}';
+    let fullTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
     
     getTime.innerText = fullTime;
+    // document.getElementById(`timer`).innerText = fullTime;
+    console.log(getTime);
+}
+
+function startTime(){
+    interval = setInterval(() => {
+    timeInterval--;
+    timeRun();
+     if (timeInterval === 0) {
+      clearInterval(interval);
+      alert("Time's up!");
+      timeInterval = 1500;
+      timeRun();
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(interval);
+}
+
+function resetTimer() {
+  clearInterval(interval);
+  timeInterval = 1500;
+  timeRun();
 }
